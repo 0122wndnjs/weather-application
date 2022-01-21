2c6423e9b3f999c78b96130cef747b05d18c5cf3
Joowon Kim <0122wndnjs@gmail.com>
2022-01-21 16:07:46 +0900

    fixing issue

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {useState, useEffect} from 'react';
 import axios from 'axios';
 
 function App() {
+  const [search, setSearch] = useState('')
   const [allData, setAllData] = useState({
     city: '',
     country: '',
@@ -16,7 +17,7 @@ function App() {
   const fetchData = async (city) => {
     try {
       const APIKEY = 'e49811732bf3ab59f71e5817eef83791'
-      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${'seoul'}&appid=${APIKEY}&units=metric`)
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`)
       await setAllData({
         city: result.data.name,
         country: result.data.sys.country,
@@ -27,11 +28,20 @@ function App() {
     }
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    fetchData(search)
+  }
+
+  const handleChange = (event) => {
+    setSearch(event.target.value)
+  }
+
   return (
     <main>
       <div className="App">
-        <form>
-          <input type='text' name='city' placeholder='City Name' />
+        <form onSubmit={handleSubmit}>
+          <input value={search} type='text' name='city' placeholder='City Name' onChange={handleChange} />
           <button for='city'>Search</button>
         </form>
         <section>
