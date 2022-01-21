2e06109d5c67a29124bdd4befddd02a16c0c3ebc
Joowon Kim <0122wndnjs@gmail.com>
2022-01-21 14:42:37 +0900

    Connecting to weather API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [allData, setAllData] = useState({
+    city: '',
+    country: '',
+    temperature: ''
+  })
+
+  useEffect(() => {
+    fetchData()
+  },[])
+
+  const fetchData = async (city) => {
+    try {
+      const APIKEY = 'e49811732bf3ab59f71e5817eef83791'
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${'seoul'}&appid=${APIKEY}&units=metric`)
+      await setAllData({
+        city: result.data.name,
+        country: result.data.sys.country,
+        temperature: result.data.main.temp
+      })
+    } catch (e) {
+      console.log('API not loaded correctly or loaded for the first time')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="App">
+        <form>
+          <input type='text' name='city' placeholder='City Name' />
+          <button for='city'>Search</button>
+        </form>
+        <section>
+          <h1>{allData.city}</h1>
+          <h2>{allData.country}</h2>
+          <h3>Temperature</h3>
+          <p>{allData.temperature}°C</p>
+        </section>
+      </div>
+    </main>
   );
 }
 
